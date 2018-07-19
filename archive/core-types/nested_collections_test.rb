32b73439b5ada5f2eb8fd0f8359da1b1b1787cfc
NataliaColome <nmcolome@gmail.com>
2018-07-19 10:52:27 -0600

    add logic  pass test

--- archive/core-types/nested_collections_test.rb
+++ archive/core-types/nested_collections_test.rb
@@ -2,51 +2,46 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class NestedCollectionsTest < Minitest::Test
-  
+
   def test_2D_array
-    skip
     numbers = [[1, 2], [9, 4, 89], 9]
-    
-    assert_equal __, numbers.first
-    assert_equal __, numbers[0][1]
-    assert_equal __, numbers[1][3]
-    assert_equal __, numbers[2]
+
+    assert_equal [1, 2], numbers.first
+    assert_equal 2, numbers[0][1]
+    assert_nil numbers[1][3]
+    assert_equal 9, numbers[2]
   end
   
   def test_2D_array_access
-    skip
     numbers = [15, [45, 0, nil], [8]]
     
-    assert_equal 15, __
-    assert_nil __
-    assert_equal 8, __
+    assert_equal 15, numbers.first
+    assert_nil numbers[1][7]
+    assert_equal 8, numbers[2][0]
   end
   
   def test_3D_array_access
-    skip
     numbers = [[9, [4], [89, 11, 42]], [23, 2], 9]
     
-    assert_equal [23, 2], __
-    assert_equal 11, __
-    assert_equal [4], __
+    assert_equal [23, 2], numbers[1]
+    assert_equal 11, numbers[0][2][1]
+    assert_equal [4], numbers[0][1]
   end
   
   def test_2D_hash
-    skip
     new_restaurants = {
       cultura: {cuisine: "Latin American", seats_available: 20}, 
       wallys: {cuisine: "Wisconsin treats", seats_available: 50}
     }
-    expected_cultura = __
-    expected_wallys_cuisine = __
+    expected_cultura = {cuisine: "Latin American", seats_available: 20}
+    expected_wallys_cuisine = "Wisconsin treats"
       
     assert_equal expected_cultura, new_restaurants[:cultura]
     assert_equal expected_wallys_cuisine, new_restaurants[:wallys][:cuisine]
-    assert_equal __, new_restaurants.values.first[:seats_available]
+    assert_equal 20, new_restaurants.values.first[:seats_available]
   end
   
   def test_3D_hash_access
-    skip
     stars = {
       proxima_centauri: {distance: 4.2, magnitudes: {
         apparent: -26.72, absolute: 4.8
@@ -54,25 +49,23 @@ class NestedCollectionsTest < Minitest::Test
       sirius_a: {distance: 8.6, magnitudes: {apparent: -1.46, absolute: 1.4}}
     }
       
-    assert_equal 8.6, __
-    assert_equal -26.72, __
-    assert_equal ({apparent: -1.46, absolute: 1.4}), __
+    assert_equal 8.6, stars[:sirius_a][:distance]
+    assert_equal -26.72, stars[:proxima_centauri][:magnitudes][:apparent]
+    assert_equal ({apparent: -1.46, absolute: 1.4}), stars[:sirius_a][:magnitudes]
   end
   
   def test_mixed_array_access
-    skip
     orders = [
       {10944 => {date: (Date.today - 1), products: ['shampoo', 'toothbrush']}},
       {10945 => {date: Date.today, products: ['handsoap']}}
     ]
     
-    assert_equal 10944, __
-    assert_equal Date.today, __
-    assert_equal 'shampoo', __
+    assert_equal 10944, orders[0].keys[0]
+    assert_equal Date.today, orders[1][10945][:date]
+    assert_equal 'shampoo', orders[0][10944][:products][0]
   end
   
   def test_mixed_hash_access
-    skip
     plants = {
       dahlia: {
         "Crichton Honey" => {
@@ -86,11 +79,11 @@ class NestedCollectionsTest < Minitest::Test
       hollyhock: ["Chater's Salmon Queen", "Blackberry"]
     }
     
-    assert_equal ['yellow', ['blue', 'rose']], __
-    assert_equal ['yellow', 'blue'], __
-    assert_equal "Blackberry", __
-    assert_equal "white", __
-    assert_equal "rose", __
+    assert_equal ['yellow', ['blue', 'rose']], plants[:dahlia]["Whimsey"]
+    assert_equal ['yellow', 'blue'], plants[:dahlia]["Crichton Honey"][:green]
+    assert_equal "Blackberry", plants[:hollyhock][1]
+    assert_equal "white", plants[:dahlia]["Crichton Honey"][:peach][2]
+    assert_equal "rose", plants[:dahlia]["Whimsey"][1][1]
   end
   
 end

7b0a3589e7ce3aadd554d45fdbef89106ad08734
NataliaColome <nmcolome@gmail.com>
2018-07-20 10:45:14 -0600

    fix bug in ReadingList

--- archive/mutation/reading_list.rb
+++ archive/mutation/reading_list.rb
@@ -1,13 +1,15 @@
+require 'pry'
 class ReadingList
   BOOKS = ["1984", "The Hobbit", "I, Robot"]
 
   attr_reader :books
 
   def initialize
-    @books = ReadingList::BOOKS
+    @books = []
+    @books.push(*ReadingList::BOOKS)
   end
 
-  def <<(book)
+  def << book
     @books << book
   end
 end
--- archive/mutation/reading_list_test.rb
+++ archive/mutation/reading_list_test.rb
@@ -11,14 +11,12 @@ class ReadingListTest < Minitest::Test
   end
 
   def test_add_books
-    skip
     list = ReadingList.new
     list << "Dune"
     assert_equal ["1984", "The Hobbit", "I, Robot", "Dune"], list.books
   end
 
   def test_different_reading_lists
-    skip
     alice = ReadingList.new
     bob = ReadingList.new
 

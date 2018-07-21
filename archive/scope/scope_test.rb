700a62a0d4d79a3e0f8141d05500c7ae309c3e46
NataliaColome <nmcolome@gmail.com>
2018-07-20 18:10:58 -0600

    Fix statements to pass test

--- archive/scope/scope_test.rb
+++ archive/scope/scope_test.rb
@@ -6,72 +6,66 @@ require 'minitest/pride'
 class ScopeTest < MiniTest::Test
 
   def test_change_variable_in_method
-    skip
     x = 1
 
     def change_x(x)
       x = 2
     end
 
-    assert_equal __, x
+    assert_equal 1, x
   end
 
   def test_change_variable_in_loop
-    skip
     x = 0
     while x < 16
       x +=1
     end
-    assert_equal __, x
+    assert_equal 16, x
   end
 
   def test_change_different_in_method
-    skip
     x = 3
     def change_x(x)
       x = 2
     end
     y = 1
     change_x(y)
-    assert_equal __, y
+    assert_equal 1, y
   end
 
   def test_variable_scope_inside_method
-    skip
     x = 1
     def change_x(x)
-      assert_equal __, x
+      assert_equal 3, x
       x = 2
-      assert_equal __, x
+      assert_equal 2, x
     end
     y = 3
     change_x(y)
-    assert_equal __, x
-    assert_equal __, y
+    assert_equal 1, x
+    assert_equal 3, y
   end
 
   def test_change_different_variable_inside_method
-    skip
     a = 1
     def change_x(x)
       x = 2
       a = 4
-      assert_equal __, a
+      assert_equal 4, a
     end
 
     change_x(a)
-    assert_equal __, a
+    assert_equal 1, a
   end
 
   def test_variable_and_method_names_are_the_same
-    skip
     a = 1
     b = 2
     def change_x(x)
-      assert_equal __, x
+      assert_equal 2, x
       x *= 2
       a = b
-      assert_equal __, a
+      assert_equal 23, a
     end
 
     def b
@@ -79,86 +73,78 @@ class ScopeTest < MiniTest::Test
     end
 
     change_x(b)
-    assert_equal __, b
-    assert_equal __, a
+    assert_equal 2, b
+    assert_equal 1, a
   end
 
   def test_block_variable
-    skip
     [1].each do |i|
-      assert_equal __, i
+      assert_equal 1, i
     end
   end
 
   def test_scope_inside_block
-    skip
     i = 2
     [1,1,1].each do |i|
-      assert_equal __, i
+      assert_equal 1, i
     end
-    assert_equal __, i
+    assert_equal 2, i
   end
 
   def test_changing_variable_inside_block
-    skip
     i = 3
     [1,2,3,4].each do |i|
       i += 1
     end
-    assert_equal __, i
+    assert_equal 3, i
   end
 
   def test_scope_of_variables_outside_block
-    skip
     x = 0
     i = 0
     [1, 2, 3, 4, 5].each do |i|
       x = i
     end
 
-    assert_equal __, x
-    assert_equal __, i
+    assert_equal 5, x
+    assert_equal 0, i
   end
 
   def test_scope_of_method_inside_block
-    skip
     def x
       20
     end
 
     [1,2,3,4,5].each do |i|
-      assert_equal __, x
+      assert_equal 20, x
     end
-    assert_equal __, x
+    assert_equal 20, x
   end
 
   def test_scope_of_local_variable_and_method_inside_block
-    skip
     def x
       20
     end
 
     [1,2,3,4,5].each do |i|
       x = 10
-      assert_equal __, x
+      assert_equal 10, x
     end
-    assert_equal __, x
+    assert_equal 20, x
   end
 
   def test_change_variable_in_block
-    skip
     x = 20
 
     [1,2,3,4,5].each do |i|
       x = 10
-      assert_equal __, x
+      assert_equal 10, x
     end
 
-    assert_equal __, x
+    assert_equal 10, x
   end
 
   def test_everything_is_x
-    skip
     x = 20
     def x
       10
@@ -166,12 +152,12 @@ class ScopeTest < MiniTest::Test
     sum = 0
     [1,2,3,4,5].each do |x|
       x = 1
-      assert_equal __, x
+      assert_equal 1, x
       sum += x
     end
 
-    assert_equal __, sum
-    assert_equal __, x
+    assert_equal 5, sum
+    assert_equal 20, x
   end
 
 end

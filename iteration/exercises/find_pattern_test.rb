2837bebb3dfde92c968190354b6fbd397d65c94c
NataliaColome <nmcolome@gmail.com>
2018-07-22 21:41:11 -0600

    Add logic to pass test

--- iteration/exercises/find_pattern_test.rb
+++ iteration/exercises/find_pattern_test.rb
@@ -35,18 +35,19 @@ class FindPatternTest < Minitest::Test
   end
 
   def test_3
-    skip
     ages = [39, 45, 29, 24, 50]
     older_than_fifty = nil
     ages.each do |age|
-      # Your Code Here
+      if age > 50
+        older_than_fifty = age
+        break
+      end
     end
 
     assert_nil older_than_fifty
   end
 
   def test_4
-    skip
     ages = {
       abdi: 39,
       hassan: 45,
@@ -56,23 +57,29 @@ class FindPatternTest < Minitest::Test
     }
     older_than_fifty = nil
     ages.each do |name, age|
-      # Your Code Here
+      if age > 50
+        older_than_fifty = name
+        break
+      end
     end
 
     assert_nil older_than_fifty
   end
 
   def test_5
-    skip
     ages = [39, 45, 29, 24, 50]
     multiple_of_three = nil
-    # Your Code Here
+    ages.each do |age|
+      if age % 3 == 0
+        multiple_of_three = age
+        break
+      end
+    end
 
     assert_equal 39, multiple_of_three
   end
 
   def test_6
-    skip
     ages = {
       abdi: 39,
       hassan: 45,
@@ -81,22 +88,30 @@ class FindPatternTest < Minitest::Test
       miguel: 50
     }
     multiple_of_three = nil
-    # Your Code Here
+    ages.each do |key, val|
+      if val % 3 == 0
+        multiple_of_three = key
+        break
+      end
     end
 
     assert_equal :abdi, multiple_of_three
   end
 
   def test_7
-    skip
     people = ["Willie", "Carmen Sandiego", "Bryan", "Faith", "Zac"]
-    # Your Code Here
+    carmen = nil
+    people.each do |name|
+      if name.include?('Carmen')
+        carmen = name
+        break
+      end
+    end
 
     assert_equal "Carmen Sandiego", carmen
   end
 
   def test_8
-    skip
     places = {
       Bangkok: "Willie",
       Santa_Fe: "Carmen Sandiego",
@@ -104,21 +119,31 @@ class FindPatternTest < Minitest::Test
       Munich: "Faith",
       Mogudishu: "Zac"
     }
-    # Your Code Here
+    where_is_carmen_sandiego = nil
+    places.each do |place, name|
+      if name.include?('Carmen')
+        where_is_carmen_sandiego = place
+        break
+      end
+    end
 
     assert_equal :Santa_Fe, where_is_carmen_sandiego
   end
 
   def test_9
-    skip
     numbers = [3, 7, 13, 11, 10, 2, 17]
-    # Your Code Here
+    even = nil
+    numbers.each do |num|
+      if num.even?
+        even = num
+        break
+      end
+    end
 
     assert_equal 10, even
   end
 
   def test_10
-    skip
     purchases = {
       "shoes" => :paid,
       "backpack" => :paid,
@@ -126,13 +151,18 @@ class FindPatternTest < Minitest::Test
       "posters" => :paid,
       "food" => :pending
     }
-    # Your Code Here
+    pending = nil
+    purchases.each do |key, value|
+      if value == :pending
+        pending = key.to_sym
+        break
+      end
+    end
 
     assert_equal :books, pending
   end
 
   def test_11
-    skip
     purchases = {
       "shoes" => :paid,
       "backpack" => :paid,
@@ -140,7 +170,13 @@ class FindPatternTest < Minitest::Test
       "posters" => :paid,
       "food" => :pending
     }
-    # Your Code Here
+    starts_with_b = nil
+    purchases.each do |key, val|
+      if key[0] == 'b'
+        starts_with_b = key
+        break
+      end
+    end
 
     assert_equal "backpack", starts_with_b
   end
